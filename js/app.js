f2f0da83427a084f9b2476ba998a722398469b38
LouisaJohnston <louisa.latham.johnston@gmail.com>
2021-02-08 12:02:30 -0800

    beginning js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-console.log('sup nerd')
+/* Constants */
+
+/* DOM References */
+const boxOne = document.querySelectorAll(".letter-input")
+
+/* Game Logic Variables and State */
+
+
+/* Functions and Game Logic */
+
+/* Event Listeners */
