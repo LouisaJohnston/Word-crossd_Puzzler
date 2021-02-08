ef91fabac8375dc59506f957e4929f4c93a7c780
LouisaJohnston <louisa.latham.johnston@gmail.com>
2021-02-08 13:31:43 -0800

    input query selectors

--- js/app.js
+++ js/app.js
@@ -1,10 +1,31 @@
 /* Constants */
 
 /* DOM References */
-const boxOne = document.querySelectorAll(".letter-input")
+const one = document.querySelector("#one");
+const two = document.querySelector("#two");
+const three = document.querySelector("#three");
+const four = document.querySelector("#four");
+const five = document.querySelector("#five");
+const six = document.querySelector("#six");
+const seven = document.querySelector("#seven");
+const eight = document.querySelector("#eight");
+const nine = document.querySelector("#nine");
+const ten = document.querySelector("#ten");
+const eleven = document.querySelector("#eleven");
+const twelve = document.querySelector("#twelve");
+const thirteen = document.querySelector("#thirteen");
+const fourteen = document.querySelector("#fourteen");
+const fifteen = document.querySelector("#fifteen");
+const sixteen = document.querySelector("#sixteen");
+const seventeen = document.querySelector("#seventeen");
+const eighteen = document.querySelector("#eighteen");
+const nineteen = document.querySelector("#nineteen");
+const twenty = document.querySelector("#twenty");
+const twentyone = document.querySelector("#twentyone");
+
+console.log(twentyone);
 
 /* Game Logic Variables and State */
-
 
 /* Functions and Game Logic */
 
