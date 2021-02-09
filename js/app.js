69fbb7dbb765e936f02b3e667f58e9fc9a94070d
LouisaJohnston <louisa.latham.johnston@gmail.com>
2021-02-09 15:35:32 -0800

    MVP

--- js/app.js
+++ js/app.js
@@ -1,60 +1,58 @@
 /***** Constants *****/
 
-
 const letterKey = {
-    one: "c",
-    two: "h",
-    three: "e",
-    four: "r",
-    five: "l",
-    six: "o",
-    seven: "w",
-    eight: "e",
-    nine: "b",
-    ten: "a",
-    eleven: "t",
-    twelve: "e",
-    thirteen: "d",
-    fourteen: "e",
-    fifteen: "v",
-    sixteen: "e",
-    seventeen: "r",
-    eighteen: "e",
-    nineteen: "e",
-    twenty: "l",
-    twentyone: "s"
-}
-
+  one: "C",
+  two: "H",
+  three: "E",
+  four: "R",
+  five: "L",
+  six: "O",
+  seven: "W",
+  eight: "E",
+  nine: "B",
+  ten: "A",
+  eleven: "T",
+  twelve: "E",
+  thirteen: "D",
+  fourteen: "E",
+  fifteen: "V",
+  sixteen: "E",
+  seventeen: "R",
+  eighteen: "E",
+  nineteen: "E",
+  twenty: "L",
+  twentyone: "S",
+};
 
 let letterInput = {
-    one: "",
-    two: "",
-    three: "",
-    four: "",
-    five: "",
-    six: "",
-    seven: "",
-    eight: "",
-    nine: "",
-    ten: "",
-    eleven: "",
-    twelve: "",
-    thirteen: "",
-    fourteen: "",
-    fifteen: "",
-    sixteen: "",
-    seventeen: "",
-    eighteen: "",
-    nineteen: "",
-    twenty: "",
-    twentyone: ""
-}
+  one: "",
+  two: "",
+  three: "",
+  four: "",
+  five: "",
+  six: "",
+  seven: "",
+  eight: "",
+  nine: "",
+  ten: "",
+  eleven: "",
+  twelve: "",
+  thirteen: "",
+  fourteen: "",
+  fifteen: "",
+  sixteen: "",
+  seventeen: "",
+  eighteen: "",
+  nineteen: "",
+  twenty: "",
+  twentyone: "",
+};
 
 /***** DOM References *****/
 // select specific input elements
 const inputs = document.querySelector(".box input");
 const grid = document.querySelector(".grid-container");
-const messageContainer = document.querySelector(".message-container")
+const messageContainer = document.querySelector(".message-container");
 
 /***** Game Logic Variables and State *****/
 
@@ -63,31 +61,23 @@ const messageContainer = document.querySelector(".message-container")
 
 //store input value for letterInput at key index
 const updateValue = (e) => {
-    let boxNumber = e.target.id
-    let inputValue = e.target.value
-    letterInput[boxNumber] = inputValue
-    // check user input against stored values for checking button
-    // for (letter in letterKey) {
-    //     if (letterInput[boxNumber] === letterKey[boxNumber]) {
-    //         console.log(`${inputValue} is correct`)
-    //     }
-    // } 
-    checkWin()
-
+  let boxNumber = e.target.id;
+  let inputValue = e.target.value;
+  letterInput[boxNumber] = inputValue;
+  checkWin();
 };
 
 const checkWin = () => {
-    for (letter in letterKey) {
-        console.log(letter)
-        if (letterKey[letter] !== letterInput[letter]) {
-            return false
-        }
-        // if (Object.values(letterKey) === Object.values(letterInput)) {
-        //     messageContainer.classList.remove("hidden")
-        //     console.log("you win")
-        // }
+  for (letter in letterKey) {
+    if (letterKey[letter] !== letterInput[letter]) {
+      return false;
     }
-    return true
+  }
+  return winState()
+};
+
+const winState = () => {
+    messageContainer.classList.remove("hidden")
 }
 
 /***** Event Listeners *****/
